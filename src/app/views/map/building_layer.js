cdbfd8a90a471fc37acca86632d840929ed1097e
Tom Allnutt <tom@greeninfo.org>
2023-12-07 10:47:41 -0800

    new nodata message for state buildings #92

--- src/app/views/map/building_layer.js
+++ src/app/views/map/building_layer.js
@@ -143,8 +143,9 @@ define([
 
     if (!_.isNumber(o.chart.lead.value) || _.isNaN(o.chart.lead.value)) {
       o.chart.lead.nodata = chartData.lead.nodata;
+      // if state building, then flag that also
+      if (building.get('state_bldg')) o.chart.lead.state_bldg = true;
     }
-
 
     o.chart.barchart = {
       value: building.get(chartData.barchart.field),
