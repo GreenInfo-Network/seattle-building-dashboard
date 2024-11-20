a984fdd78d11c56eaff34b2a81170153a54c11ef
Alexander Parlato <alexparlato@stamen.com>
2024-11-20 13:46:24 -1000

    benchmark overview page

--- src/app/views/scorecards/charts/use_types.js
+++ src/app/views/scorecards/charts/use_types.js
@@ -99,7 +99,10 @@ define([
         .attr('width', width)
         .attr('height', height)
         .append('g')
-        .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
+        .attr(
+          'transform',
+          'translate(' + width / 2 + ',' + outerHeight / 2 + ')'
+        );
 
       // Compute the position of each group on the pie:
       var pie = d3.pie().value(function (d) {
